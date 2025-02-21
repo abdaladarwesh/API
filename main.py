2cb3f924196a0eb1546a79ce2e0beb70edc2608e
abdallah <abdaladarwesh63@gmail.com>
2025-02-21 15:17:12 +0200

    adding checking and del method and enhanced put method

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from flask import *
-from flask_restful import Api, Resource, reqparse
+from flask import Flask
+from flask_restful import reqparse, Resource,Api, abort
 
 app = Flask(__name__)
 api = Api(app)
-videoArgs = reqparse.RequestParser()
-videoArgs.add_argument("name", type=str, help='this is name')
-videoArgs.add_argument('likes', type=int, help='this is likes')
 
+addVids = reqparse.RequestParser()
+addVids.add_argument("name", type=str, required=True,  help='name type is not right or its requierd')
+addVids.add_argument("views", type=int, required=True,  help='views type is not right or its requierd')
 
-class helloWorld(Resource):
-    def get(self): 
-        return {'message':'hello world'}
+videos = {}
+def check(video_id):
+    if video_id not in videos:
+        abort(404, message="the video you request is not exsit......")
 
-class addvids(Resource):
-    def put(self):
-        args = videoArgs.parse_args()
-        return {'1':args}
+def checkForExsit(video_id):
+    if video_id in videos:
+        abort(409, message="the video you request to put is already exsit......")
 
-api.add_resource(addvids, "/vid")
-api.add_resource(helloWorld, "/")
+class vids(Resource):
+    def get(self, video_id):
+        check(video_id)
+        return videos[video_id] , 200
+    
+    def put(self, video_id):
+        checkForExsit(video_id)
+        args = addVids.parse_args()
+        videos[video_id] = args
+        return videos[video_id] , 201
+    
+    def delete (self, video_id):
+        check(video_id)
+        del videos[video_id]
+        return '', 204
+    
+    
+api.add_resource(vids, "/vid/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,5 +1,19 @@
 import requests
 
 base = "http://127.0.0.1:5000/"
-respond = requests.put(base + "vid", {'name':'gg', "likes":"876"})
-print(respond.json())
+putReq1 = requests.put(base + "vid/1", {"views":15, "name":"abdallah"})
+putReq2 = requests.put(base + "vid/2", {"name":"laila", "views":66})
+print(putReq1.json())
+print(putReq2.json())
+input()
+getreq1 = requests.get(base + "vid/1")
+getreq2 = requests.get(base + "vid/2")
+print(getreq1.json())
+print(getreq2.json())
+delreq1 = requests.delete(base + "vid/1")
+getreq1 = requests.get(base + "vid/1")
+getreq2 = requests.get(base + "vid/2")
+print(getreq1.json())
+print(getreq2.json())
+
+
